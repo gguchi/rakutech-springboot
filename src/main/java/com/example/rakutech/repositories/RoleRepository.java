0aee3be16e971c902761baa9e56ec9dcd49a1d4e
james-99k <jamesgasty@gmail.com>
2021-07-29 18:17:57 +0800

    security

---
+++ src/main/java/com/example/rakutech/repositories/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.example.rakutech.repositories;
+
+import com.example.rakutech.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long>  {
+    Role findByName(String name);
+
+}
