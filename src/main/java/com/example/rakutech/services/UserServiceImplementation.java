df2df5312d9239d0e8336c7d68ee8ce7ffbf5bd2
james-99k <jamesgasty@gmail.com>
2021-08-04 21:00:10 +0800

    JWT

--- src/main/java/com/example/rakutech/services/UserServiceImplementation.java
+++ src/main/java/com/example/rakutech/services/UserServiceImplementation.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
@@ -21,6 +22,7 @@ import java.util.List;
 public class UserServiceImplementation implements UserService, UserDetailsService {
     private final UserRepository userRepository;
     private final RoleRepository roleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -39,6 +41,7 @@ public class UserServiceImplementation implements UserService, UserDetailsServic
     @Override
     public User saveUser(User user) {
         log.info("Saving new user {} to the database",user.getName());
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
